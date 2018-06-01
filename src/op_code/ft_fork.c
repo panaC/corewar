3c0c609508ff7efef75c452c3bd9342810b0902f
pleroux <p.leroux@gmx.com>
2018-06-01 12:41:53 +0200

    push pc pierre

--- src/op_code/ft_fork.c
+++ src/op_code/ft_fork.c
@@ -6,7 +6,7 @@
 /*   By: msukhare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/20 02:26:04 by msukhare          #+#    #+#             */
-/*   Updated: 2018/05/20 06:33:48 by msukhare         ###   ########.fr       */
+/*   Updated: 2018/05/24 12:23:35 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ static void		ft_del_and_exit(t_env *env)
 	exit(-1);
 }
 
+//le header de la fct n'inclue pas env
+//cf add.c -> int			op_add(void *v, t_list *l, t_uint8 *b)
 void		ft_fork(t_env *env, t_process *p)
 {
 	t_process	*new;
@@ -43,6 +45,7 @@ void		ft_fork(t_env *env, t_process *p)
 	new->numero = p->numero;
 	new->carry = p->carry;
 	new->live = p->live;
+	//Il faut recuperer le PC en argument de l'op code grace a p->arg
 	new->pc = p->pc;
 	while (p->next)
 		p = p->next;
