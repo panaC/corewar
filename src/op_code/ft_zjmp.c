27b09333fe7ea972abb6de0fe28ab540ceb8328c
sukharev <msukhare@student.42.fr>
2018-06-05 14:38:15 +0200

    ft_zjmp

---
+++ src/op_code/ft_zjmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_zjmp.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msukhare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/28 14:07:20 by msukhare          #+#    #+#             */
+/*   Updated: 2018/06/05 11:20:09 by msukhare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int		op_zjmp(void *e)
+{
+	t_process	*p;
+
+	p = ((t_env*)e)->current_process;
+	if (p->carry != 1)
+		return (FALSE);
+	p->pc = p->pc + (p->op.arg[0] % IDX_MOD);
+	return (TRUE);
+}
